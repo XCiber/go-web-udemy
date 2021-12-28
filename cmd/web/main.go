dfedcd2a5de2980644ab3768b6ece863ee58ed4e
Andrey Kolesnichenko <andrey.kolesnichenko@exness.com>
2021-12-28 13:00:45 +0200

    Use templates

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"github.com/XCiber/go-web-udemy/pkg/config"
 	"github.com/XCiber/go-web-udemy/pkg/handlers"
+	"github.com/XCiber/go-web-udemy/pkg/render"
 	"log"
 	"net/http"
 )
@@ -12,11 +14,25 @@ const port = 8080
 // main is the app entry point
 func main() {
 
-	http.HandleFunc("/", handlers.Home)
-	http.HandleFunc("/about", handlers.About)
+	var app config.AppConfig
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatalf("Can't get templates cache: %v", err)
+	}
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
+	render.NewTemplate(&app)
+
+	http.HandleFunc("/", handlers.Repo.Home)
+	http.HandleFunc("/about", handlers.Repo.About)
 
 	log.Printf("Starting app server on port %d", port)
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	err = http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 	if err != nil {
 		log.Fatalf("Could not start server on port %d: %v", port, err)
 	}
--- pkg/render/render.go
+++ pkg/render/render.go
@@ -1,15 +1,79 @@
 package render
 
 import (
+	"bytes"
+	"github.com/XCiber/go-web-udemy/pkg/config"
 	"html/template"
 	"log"
 	"net/http"
+	"path/filepath"
 )
+
+var functions = template.FuncMap{}
+
+var app *config.AppConfig
+
+func NewTemplate(a *config.AppConfig) {
+	app = a
+}
 
 // RenderTemplate renders templates from "templates" directory
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
-	if err := parsedTemplate.Execute(w, nil); err != nil {
+
+	var tc map[string]*template.Template
+
+	if app.UseCache {
+		tc = app.TemplateCache
+	} else {
+		tc, _ = CreateTemplateCache()
+	}
+
+	t, ok := tc[tmpl]
+	if !ok {
+		log.Fatalf("Cannot find template %s", tmpl)
+	}
+
+	buf := new(bytes.Buffer)
+	_ = t.Execute(buf, nil)
+	_, err := buf.WriteTo(w)
+	if err != nil {
 		log.Printf("error parsing template: %v", err)
 	}
+}
+
+// CreateTemplateCache return the map with templates
+func CreateTemplateCache() (map[string]*template.Template, error) {
+
+	myCache := map[string]*template.Template{}
+
+	pages, err := filepath.Glob("./templates/*.page.tmpl")
+	if err != nil {
+		return myCache, err
+	}
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
+		if err != nil {
+			return myCache, err
+		}
+
+		matches, err := filepath.Glob("./templates/*.layout.tmpl")
+		if err != nil {
+			return myCache, err
+		}
+
+		if len(matches) > 0 {
+			ts, err = ts.ParseGlob("./templates/*.layout.tmpl")
+			if err != nil {
+				return myCache, err
+			}
+		}
+
+		myCache[name] = ts
+	}
+
+	return myCache, nil
+
 }
