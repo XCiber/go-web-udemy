b1fabf2557052090e4a09db0ba93b62c9e9fae4f
Andrey Kolesnichenko <andrey.kolesnichenko@exness.com>
2021-12-29 15:34:12 +0200

    Work with data in session

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -25,6 +25,8 @@ func NewHandlers(r *Repository) {
 
 // Home is the home page handler
 func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	remoteIP := r.RemoteAddr
+	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
 	render.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{})
 }
 
@@ -33,6 +35,9 @@ func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 	// perform some business logic
 	stringMap := make(map[string]string)
 	stringMap["test"] = "Hello, again."
+
+	remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
+	stringMap["remote_ip"] = remoteIP
 
 	// send the data to the template
 	render.RenderTemplate(w, "about.page.tmpl", &models.TemplateData{
